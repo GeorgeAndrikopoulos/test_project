53365a7aeeb761289eab91f9b97dc4381f52f702
GeorgeAndrikopoulos <geoandriofficial@gmail.com>
2023-11-21 15:04:56 +0200

    First Test Commit

---
+++ test_project_1.c
@@ -0,0 +1,15 @@
+/*
+ * test_project_1.c
+ *
+ *  Created on: Nov 21, 2023
+ *      Author: georgeand
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("Catch me if you can.");
+}
